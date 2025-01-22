a6b6008cc3016f0eee78ed7bbd22435158f17e14
Thomas Noakes <tgn203@exeter.ac.uk>
2025-01-22 01:36:33 +0000

    Matrix subtraction

--- Week 2/main.py
+++ Week 2/main.py
@@ -24,6 +24,25 @@ def matrix_addition(mat_a: list, mat_b: list):
     return mat_ans
 
 
+def matrix_subtraction(mat_a: list, mat_b: list):
+    """Subtract two matrices of the same dimensions"""
+
+    # Ensure matrices are of the same dimensions
+    dim_a = (len(mat_a), len(mat_a[0]))
+    dim_b = (len(mat_b), len(mat_b[0]))
+    if dim_a != dim_b:
+        raise ValueError("Matrices must be of the same dimensions")
+
+    # Create and fill the output matrix
+    mat_ans: list = []
+    for i, row in enumerate(mat_a):
+        mat_ans.append([])
+        for j in range(len(row)):
+            mat_ans[i].append(mat_a[i][j] - mat_b[i][j])
+
+    return mat_ans
+
+
 def main():
     """
     Matrix Manipulation
@@ -37,6 +56,7 @@ def main():
     print("-" * 50)
 
     print(f"A + B = {matrix_addition(a, b)}")
+    print(f"A - B = {matrix_subtraction(a, b)}\n")
 
 
 if __name__ == "__main__":
